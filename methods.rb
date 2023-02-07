860e53abd30b855e4af4160e0ffedb736b64776e
Chepkoech1 <sandrachepkoech60@gmail.com>
2023-02-07 15:34:31 +0300

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+def greet_programmer()
+    puts "Hello, programmer!"
+end
+
+def greet(name = "Sandra")
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(a, b)
+    return a + b
+end
+puts add(1, 2)
+
+def halve(a = 7)
+    if a.class != Integer
+        return nil
+    end
+    a/2
+end
+
+  
+
+
